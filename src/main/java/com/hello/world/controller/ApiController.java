9ebd0e67b44f729f150174dcab6d0ef51a3c9b41
Syed Ahmed <syed.sakeeb.ahmed@gmail.com>
2025-05-07 15:49:53 -0400

    first commit

---
+++ src/main/java/com/hello/world/controller/ApiController.java
@@ -0,0 +1,21 @@
+package com.hello.world.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class ApiController {
+    @GetMapping
+    @CrossOrigin(origins = "http://localhost:5173")
+    public Data sayHello() {
+        return new Data("Hello, World V10!", 42);
+    }
+}
+
+
+
+
+
